a60ad290dacb4114173d3971250db22a16b8d803
Himanshu Singh <himanshusingh6612@gmail.com>
2025-07-23 23:37:17 +0530

    commit for props

--- booklistapp/src/index.js
+++ booklistapp/src/index.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
+const image='https://m.media-amazon.com/images/I/71FvrzgqDqL._SY342_.jpg';
+const title= 'Parmar SSC Fatman | GK/GS Theory Book | English Medium' ;
+const author='Parmar Sir'
+const someFunction = (param1, param2)=> {
+  console.log(param1,param2)
+}
+someFunction(1+3,'ji')
+
+
+
+
+
 
 function BookList (){
   return <section className='bookList'>
     
-    <Book/>
-    <Book/>
-    <Book/>
+    <Book name="Himanshu"/>
+    <Book name="Amit" number={123}/>
+    <Book  testMessage="testing this"/>
 
   </section>
 }
-const Book = ()=> {
+const Book = (props)=> {
+  console.log(props);
   return  <article  className='book'>
-<Image/>
-<Title/>
-<Author/>
+<img src={image}
+alt='Book title 1'
+/>
+<h2>{title}</h2>
+<h2>{author}</h2>
   </article> 
 }
-const Image = ()=> <img src='https://m.media-amazon.com/images/I/71FvrzgqDqL._SY342_.jpg'
+/** 
+const Image = ()=> <img src={image}
 alt='Book title 1'
 />;
-const Title = ()=> <h2>Parmar SSC Fatman | GK/GS Theory Book | English Medium </h2>;
+const Title = ()=> <h2>{title}</h2>;
 const Author = ()=> {
-  return <h2>Parmar Sir</h2>;
+  return <h2>{author}</h2>;
   
 };
+*/
 
 
 
