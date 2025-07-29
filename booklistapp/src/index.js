0d81728c2b4c657dbd98fc6dad4b6f82a5de2833
Himanshu Singh <himanshusingh6612@gmail.com>
2025-07-30 01:53:32 +0530

    used props, keys and spread function

--- booklistapp/src/index.js
+++ booklistapp/src/index.js
@@ -69,6 +69,7 @@ alt='Book title 1'
 {children}
   </article> 
 }
+
 const firstBook ={
   author:"JP morgan",
   title : "How to handle money ",
@@ -76,7 +77,26 @@ const firstBook ={
 
 
 }
-
+const secondBook = {
+  author:"Himanshu Singh",
+  title :"How not to be poor",
+  image : "/image/mybook1.avif",
+}
+/** now use many book in a list  */
+const mybookList = [
+  {
+     author:"JP chase",
+  title : "How to handle money ",
+  image : "/image/jp.jpg",
+  id: 1,
+  },
+  {
+     author:"Himanshu Singh",
+  title :"How not to be poor",
+  image : "/image/mybook1.avif",
+  id:2,
+  }
+]
 /**
  * if image is inside public folder then path should be like this "/image/jp.jpg"
  * if inside src folder then we have to import this first import jpImage from './image/jp.jpg'
@@ -93,9 +113,88 @@ function NewBookList(){
 
    <Book author={firstBook.author} title={firstBook.title}  img={firstBook.image}/>
     <Book author={firstBook.author} title={firstBook.title}  img={firstBook.image}/>
+     <Book author={secondBook.author} title={secondBook.title}  img={secondBook.image}/>
  </section>
 }
+/** map method - creates a new Array from calling a function for every array element  */
+const names= ['jhnon','peter'];
+const newNames= names.map((name)=>{
+  console.warn(name);
+  return <h1>{name}</h1>
+})
+//
+// function MyBookList (){
+//   return <section className='MYBookList'>
+// {/* <h1>{newNames}</h1> */}
+// {
+//   mybookList.map((book)=>{
+//      const {image,title,author,id}=book
+//     console.log(book)
+//     return (
 
+//       <Book img= {image} author={author} title={title } key ={id}></Book>
+
+//     );
+//   })
+// }
+//   </section>
+// }
+//////
+/** key Prop  */
+
+// const MyBookList = ()=>{
+//   return (
+//     <section className='MyBookList'>
+//       {
+//         mybookList.map((book)=>{
+//           return <MyBook book={book} key={book.id} />
+//         })
+//       }
+//     </section>
+//   )
+// }
+
+//now using spread operator 
+const MyBookList =()=> {
+  return <section className='MyBookList'>
+     <MyEvent/>
+    { mybookList.map((book) => {
+      return <MyBook {...book} key={book.id}/>
+    })
+
+    }
+  </section>
+}
+//event handling in React
+const MyEvent = () =>{
+  const handleFormInput = ()=>{
+    console.log('handle form input');
+  };
+  const handleButtonClick = ()=>{
+    alert('handle button input');
+  };
+return (
+<section className='myEvent'>
+  <form>
+    <h2>example form</h2>
+    <input type='text' name="enter" onChange={handleFormInput} style={{margin:'1rem 0'}} />
+    <button onClick={handleButtonClick}>click me </button>
+  </form>
+</section>
+);
+};
+const MyBook = (props)=> {
+  console.log('making a comp for reuse ')
+  
+  const {image,author,title,id}=props
+  return <section className='MyBook'>
+    <h2>test purpose</h2>
+    <img src={image} alt={title}/>
+    <h2>{title}</h2>
+    <h4>{author}</h4>
+  </section>
+
+}
 
 
 
@@ -105,6 +204,7 @@ root.render(
  <React.StrictMode>
     <BookList />
     <NewBookList />
+    <MyBookList/>
     </React.StrictMode>
 
  
